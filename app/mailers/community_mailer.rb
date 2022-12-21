db6ffc855981c4d509ccfa1057185363b0c3df55
Ali Hassan Mirza <Ali.Hassan.Mirzaa@gmail.com>
2022-12-21 12:56:37 +0400

    skip email for confirmation test env

--- app/mailers/community_mailer.rb
+++ app/mailers/community_mailer.rb
@@ -24,7 +24,7 @@ class CommunityMailer < ActionMailer::Base
 
       begin
         token = AuthToken.create_unsubscribe_token(person_id: person.id).token
-        # Weekly email newsletter.
+        # Weekly email newsletter
         #MailCarrier.deliver_now(CommunityMailer.community_updates(recipient: person, community: community, listings: listings_to_send, unsubscribe_token: token))
         MailCarrier.deliver_now(
           CommunityMailer.community_updates(
