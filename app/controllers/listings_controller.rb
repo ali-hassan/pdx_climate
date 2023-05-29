5f9a002cbdf01ce03de1f0612769b689e2cedc59
Ali Hassan Mirza <Ali.Hassan.Mirzaa+100@gmail.com>
2023-05-29 19:07:15 +0500

    update_listing_for_expiry method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -244,6 +244,13 @@ class ListingsController < ApplicationController
   #
   #update all for expiry of listings
   #
+  def update_listing_for_expiry
+    #update all for expiry of listings
+    @current_community = Community.first
+    Listing.all.map do |listing|
+      Delayed::Job.enqueue(ListingExpireNotificationJob.new(listing, @current_community), 5, (listing.valid_until -  30.minutes))  if listing.valid_until.present? && listing.valid_until > Time.now
+    end
+  end
   def update_expiry
     #update all for expiry of listings
     @current_community = Community.first
